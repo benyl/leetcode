b907d1e63c3cf03ad5d3670697bca6c581db84f4
benyl <beny.yb.luo@gmail.com>
2013-06-04 18:52:47 -0400

    update 2013/06/04

---
+++ Sum Root to Leaf Numbers.cc
@@ -0,0 +1,85 @@
+/*
+Sum Root to Leaf Numbers
+
+http://leetcode.com/onlinejudge#question_129
+
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25.
+*/
+
+// push root to path (not null)
+// for each node in stack, add to path (not null)
+    // if not left and right child, is leaf node
+        // print path
+        // while path.last node is right child of it's parent, pop path.last from path
+        // while path.last node has not right child, pop path.last from path
+        // pop path.last (the first left child in path) from path, push the right child to path
+    // if has left child, push left child to stack
+    // else if has right child, push right child to stack
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode *root) {
+        // use DFS search every leaf to node path
+        int sum = 0;
+        vector<TreeNode *> path;
+        
+        if(!root) return sum;
+        
+        path.push_back(root);
+       
+        while(path.size()!=0) {
+            if(path.back()->left) {
+                path.push_back(path.back()->left);
+            } else if(path.back()->right){
+                path.push_back(path.back()->right);
+            } else {
+                // if leaf node, print path and then pop nodes
+                int num = 0;
+                for(int i=0; i<path.size(); ++i)
+                    num = num*10 + path[i]->val;
+                sum += num;
+                
+                // pop nodes if node is right child
+                int last = path.size() - 1;
+                while(last!=0) {
+                    if((path[last] == path[last-1]->right) || 
+                    (!path[last-1]->right)) {
+                        path.pop_back();
+                        last--;
+                    } else 
+                        break;
+                }
+                
+                path.pop_back();
+                
+                if(path.size()!=0)
+                    path.push_back(path.back()->right);
+            } // end of : if(path.back()->left) {
+        } // end of : while(path.size()!=0) {
+        
+        return sum;
+    }
+};
---
+++ Surrounded Regions.cc
@@ -0,0 +1,65 @@
+/*
+Surrounded Regions
+
+http://leetcode.com/onlinejudge#stkstion_130
+
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region .
+
+For example,
+
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+*/
+
+class Solution {
+public:
+    void solve(vector<vector<char> > &board) {
+        if(board.size()==0 || board[0].size()==0) return;
+        
+        int m = board.size();
+        int n = board[0].size();
+        stack<pair<int, int> > stk;
+        
+        // mark all edge 'O' to '+'
+        for(int i=0; i<m; ++i) {
+            if(board[i][0]=='O') { board[i][0] = '+'; stk.push(make_pair(i,0)); }
+            if(board[i][n-1]=='O') { board[i][n-1] = '+'; stk.push(make_pair(i,n-1)); }
+        }
+        for(int j=0; j<n; ++j) {
+            if(board[0][j]=='O') { board[0][j] = '+'; stk.push(make_pair(0,j)); }
+            if(board[m-1][j]=='O') { board[m-1][j] = '+'; stk.push(make_pair(m-1,j)); }
+        }
+        
+        // mark '+' surrounded 'O' to '+'
+        while(stk.size() != 0) {
+            int i = stk.top().first;
+            int j = stk.top().second;
+            stk.pop();
+            
+            if(i<m-1 && board[i+1][j]=='O') { board[i+1][j] = '+'; stk.push(make_pair(i+1,j)); }
+            if(i>0 && board[i-1][j]=='O') { board[i-1][j] = '+'; stk.push(make_pair(i-1,j)); }
+            if(j<n-1 && board[i][j+1]=='O') { board[i][j+1] = '+'; stk.push(make_pair(i,j+1)); }
+            if(j>0 && board[i][j-1]=='O') { board[i][j-1] = '+'; stk.push(make_pair(i,j-1)); }
+        }
+        
+        // mark all other 'O' to 'X'
+        for(int i=0; i<m; ++i)
+        for(int j=0; j<n; ++j)
+            board[i][j] = (board[i][j]=='O') ? 'X' : board[i][j];
+        
+        // mark all '+' to 'O'
+        for(int i=0; i<m; ++i)
+        for(int j=0; j<n; ++j)
+            board[i][j] = (board[i][j]=='+') ? 'O' : board[i][j];
+    }
+};
