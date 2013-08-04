9ff0d7d76dab65dcaffdfa91c4cd6a5231f04a83
benyl <beny.yb.luo@gmail.com>
2013-08-04 17:39:29 -0400

    update new version

--- Interleaving String.cc
+++ Interleaving String.cc
@@ -81,3 +81,31 @@ void main()
     
     getchar();
 }
+
+
+// ================================================================
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.size() + s2.size() != s3.size()) return false;
+        if(s1.size() == 0) return s2 == s3;
+        if(s2.size() == 0) return s1 == s3;
+        
+        vector< vector<bool> > dp(s1.size()+1, vector<bool>(s2.size()+1, true));
+        
+        for(int i=1; i<=s1.size(); ++i)
+                dp[i][0] = (s1[i-1] == s3[i-1]);
+        for(int j=1; j<=s2.size(); ++j)
+                dp[0][j] = (s2[j-1] == s3[j-1]);
+                
+        for(int i=1; i<=s1.size(); ++i)
+        for(int j=1; j<=s2.size(); ++j)
+            dp[i][j] = ((s1[i-1] == s3[i+j-1]) && dp[i-1][j]) || 
+                       ((s2[j-1] == s3[i+j-1]) && dp[i][j-1]);
+                       
+        return dp[s1.size()][s2.size()];
+    }
+};
+
+
